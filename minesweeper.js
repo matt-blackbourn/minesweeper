586bcbb8eda653177a61cd604056a4e5116d545f
matt-blackbourn <matt.blackie114@gmail.com>
2020-09-18 09:48:27 +1200

    add playAgain fuctionality

--- minesweeper.js
+++ minesweeper.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-
 let board = {}
 
 function generateBoard(n){
    board.cells = []
    let rowCount = 0
    for(let i = 0; i < n*n; i++){
-
       board.cells[i] = {
          row: Math.floor(rowCount),
          col: (n + i) % n,
@@ -20,43 +18,32 @@ function generateBoard(n){
    }
 }
 
-
 function startGame () {
-   
    generateBoard(6)
-   
-  // Don't remove this function call: it makes the game work!
-  for(let i = 0; i < board.cells.length; i++){
-     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
-  }
-   
-  lib.initBoard()
-
+   for(let i = 0; i < board.cells.length; i++){
+      board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+   }
+   lib.initBoard()
    document.querySelector(".board").addEventListener("click", checkForWin)
    document.querySelector(".board").addEventListener("contextmenu", checkForWin)
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
 function checkForWin () {
    for(let i = 0; i < board.cells.length; i++){
       if(board.cells[i].isMine === true && board.cells[i].isMarked === false) return
       if(board.cells[i].isMine === false && board.cells[i].hidden === true) return
    }
    lib.displayMessage('You win!')
+   playAgain()
 } 
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
- // 
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
-
+function playAgain(){
+   let button = document.querySelector(".button")
+   button.classList.remove("button")
+   button.addEventListener("click", function(){
+      location.reload()
+   })
+}
 
 function countSurroundingMines (cell) {
    var surrounding = lib.getSurroundingCells(cell.row, cell.col)
