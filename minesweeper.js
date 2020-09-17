de77b17f8ac8b51160d33e3bebd49bc72da8070e
matt-blackbourn <matt.blackie114@gmail.com>
2020-09-17 17:20:01 +1200

    add basic generate board function

--- minesweeper.js
+++ minesweeper.js
@@ -1,119 +1,148 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {
-   cells: [
-      {
-         row: 0, 
-         col: 0, 
-         isMine: true, 
-         hidden: true
-      },
-      {
-         row: 0, 
-         col: 1, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 0, 
-         col: 2, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 0, 
-         col: 3, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 1, 
-         col: 0, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 1, 
-         col: 1, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 1, 
-         col: 2, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 1, 
-         col: 3, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 2, 
-         col: 0, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 2, 
-         col: 1, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 2, 
-         col: 2, 
-         isMine: true, 
-         hidden: true
-      },
-      {
-         row: 2, 
-         col: 3, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 3, 
-         col: 0, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 3, 
-         col: 1, 
-         isMine: true, 
-         hidden: true
-      },
-      {
-         row: 3, 
-         col: 2, 
-         isMine: false, 
-         hidden: true
-      },
-      {
-         row: 3, 
-         col: 3, 
-         isMine: false, 
-         hidden: true
-      },
-   ]
+// var board = {
+//    cells: [
+//       {
+//          row: 0, 
+//          col: 0, 
+//          isMine: true, 
+//          hidden: true
+//       },
+//       {
+//          row: 0, 
+//          col: 1, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 0, 
+//          col: 2, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 0, 
+//          col: 3, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 1, 
+//          col: 0, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 1, 
+//          col: 1, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 1, 
+//          col: 2, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 1, 
+//          col: 3, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 2, 
+//          col: 0, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 2, 
+//          col: 1, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 2, 
+//          col: 2, 
+//          isMine: true, 
+//          hidden: true
+//       },
+//       {
+//          row: 2, 
+//          col: 3, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 3, 
+//          col: 0, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 3, 
+//          col: 1, 
+//          isMine: true, 
+//          hidden: true
+//       },
+//       {
+//          row: 3, 
+//          col: 2, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//       {
+//          row: 3, 
+//          col: 3, 
+//          isMine: false, 
+//          hidden: true
+//       },
+//    ]
+// }
+
+
+let board = {}
+
+function generateBoard(n){
+  
+   board.cells = []
+   let rowCount = 0
+   
+   for(let i = 0; i < n*n; i++){
+      
+      let rowCount
+      if(i < 5) rowCount = 0
+      if(i > 4 && i < 10) rowCount = 1
+      if(i > 9 && i < 15) rowCount = 2
+      if(i > 14 && i < 20) rowCount = 3
+      if(i > 19 && i < 25) rowCount = 4
+   
+      board.cells[i] = {
+         row: rowCount,
+         col: (n + i) % n,
+         hidden: true,
+         isMine: true,
+         isMarked: false
+      }
+   }
 }
 
+
 function startGame () {
+   
+   generateBoard(5)
+   
   // Don't remove this function call: it makes the game work!
   for(let i = 0; i < board.cells.length; i++){
      board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
+   
   lib.initBoard()
-  document.querySelector(".board").addEventListener("click", function(e){
-      checkForWin()
-   })
-   document.querySelector(".board").addEventListener("contextmenu", function(e){
-      checkForWin()
-   })
+
+   document.querySelector(".board").addEventListener("click", checkForWin)
+   document.querySelector(".board").addEventListener("contextmenu", checkForWin)
 }
 
 // Define this function to look for a win condition:
